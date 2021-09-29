42023eae1aef2ca5df22e146754917e9a04a36b6
abcbcdyl <86539734+abcbcdy@users.noreply.github.com>
2021-09-29 16:51:34 +0900

    Update RequestDTO.java

--- src/main/java/com/iu/s5/board/request/RequestDTO.java
+++ src/main/java/com/iu/s5/board/request/RequestDTO.java
@@ -1,5 +1,15 @@
 package com.iu.s5.board.request;
 
-public class RequestDTO {
+import com.iu.s5.board.BoardDTO;
 
+public class RequestDTO extends BoardDTO{
+	
+	private String restName;
+	private String restLocation;
+	private String restPhone;
+	private String restCategory;
+	private String restTime;
+	private String holiday;
+	private String requestAccept;
+	
 }
