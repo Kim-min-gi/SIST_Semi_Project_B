cf8f72c9c94fa307b98a845330da4bfaba8df0c3
sj2sj <waterttug@naver.com>
2021-09-29 11:19:23 +0900

    init Community

--- src/main/java/com/iu/s5/board/BoardDTO.java
+++ src/main/java/com/iu/s5/board/BoardDTO.java
@@ -1,5 +1,58 @@
 package com.iu.s5.board;
 
-public class BoardDTO {
+import java.sql.Date;
 
+public class BoardDTO {
+	private Long num; //글번호
+	private String id; //멤버아이디
+	private String writer; //멤버닉네임(글작성자)
+	private String title; //글제목
+	private String contents; //글내용
+	private Date regDate; //글작성일
+	private Long hits; //조회수
+	
+	
+	public Long getNum() {
+		return num;
+	}
+	public void setNum(Long num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public Long getHits() {
+		return hits;
+	}
+	public void setHits(Long hits) {
+		this.hits = hits;
+	}
+	
 }
