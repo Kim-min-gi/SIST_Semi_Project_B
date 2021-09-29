3267fa6bce99ba3ad2ecf7a594dab9182d63bc3d
sj2sj <waterttug@naver.com>
2021-09-29 11:36:34 +0900

    list

--- src/main/java/com/iu/s5/board/community/CommunityController.java
+++ src/main/java/com/iu/s5/board/community/CommunityController.java
@@ -1,8 +1,21 @@
 package com.iu.s5.board.community;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/community/**")
 public class CommunityController {
 
+	
+	@GetMapping("list")
+	public ModelAndView getList() throws Exception {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName(null);
+		return mv;
+	}
+	
 }
