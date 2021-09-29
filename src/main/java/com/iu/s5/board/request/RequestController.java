6628026149a79e47807a89cb5c8111bed5e1ea73
abcbcdyl <86539734+abcbcdy@users.noreply.github.com>
2021-09-29 17:32:30 +0900

    getList

--- src/main/java/com/iu/s5/board/request/RequestController.java
+++ src/main/java/com/iu/s5/board/request/RequestController.java
@@ -1,17 +1,28 @@
 package com.iu.s5.board.request;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.iu.s5.board.BoardDTO;
+
 @Controller
 @RequestMapping("/request/**")
 public class RequestController {
 	
+	@Autowired
+	private RequestService requestService;
+	
 	@GetMapping("list")
 	public ModelAndView getList() throws Exception {
 		ModelAndView mv = new ModelAndView();
+		List<BoardDTO> ar = requestService.getList();
+		
+		mv.addObject("list", ar);
 		mv.setViewName("board/list");
 		
 		return mv;
--- src/main/java/com/iu/s5/board/request/RequestService.java
+++ src/main/java/com/iu/s5/board/request/RequestService.java
@@ -1,8 +1,52 @@
 package com.iu.s5.board.request;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class RequestService {
+import com.iu.s5.board.BoardDTO;
+import com.iu.s5.board.BoardService;
 
+@Service
+public class RequestService implements BoardService {
+	
+	@Autowired
+	private RequestDAO requestDAO;
+
+	@Override
+	public List<BoardDTO> getList() throws Exception {
+		
+		//페이징 처리
+		
+		
+		return requestDAO.getList();
+	}
+
+	@Override
+	public BoardDTO getSelect(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setInsert(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDelete(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
 }
