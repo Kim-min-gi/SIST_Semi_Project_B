32add7c66a36a7b3371c6405c2918bf882fae4af
sj2sj <waterttug@naver.com>
2021-09-29 12:38:58 +0900

    BoardDAO, Service 수정

--- src/main/java/com/iu/s5/board/community/CommunityService.java
+++ src/main/java/com/iu/s5/board/community/CommunityService.java
@@ -1,7 +1,45 @@
 package com.iu.s5.board.community;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.iu.s5.board.BoardDTO;
 import com.iu.s5.board.BoardService;
 
-public class CommunityService extends BoardService {
+@Service
+public class CommunityService implements BoardService {
 
+	@Override
+	public List<BoardDTO> getList() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDTO getSelect(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setInsert(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDelete(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
 }
