e8d0180058be79e20eabea801c4f3e874b58267f
Hyunmin Choi <74778827+hymn9611@users.noreply.github.com>
2021-09-29 15:15:20 +0900

    member부분 DTO~Controller 메서드 선언

--- src/main/java/com/iu/s5/member/MemberDAO.java
+++ src/main/java/com/iu/s5/member/MemberDAO.java
@@ -1,5 +1,27 @@
 package com.iu.s5.member;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAO {
 
+	public int setJoin(MemberDTO memberDTO) throws Exception {
+		return 0;
+	}
+	
+	public MemberDTO getLogin(MemberDTO memberDTO) throws Exception {
+		return null;
+	}
+	
+	public MemberDTO getIdCheck(MemberDTO memberDTO) throws Exception {
+		return null;
+	}
+	
+	public int setDelete(MemberDTO memberDTO) throws Exception {
+		return 0;
+	}
+	
+	public int setUpdate(MemberDTO memberDTO) throws Exception {
+		return 0;
+	}
 }
