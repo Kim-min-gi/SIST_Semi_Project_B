0d54ce29b45c48c8b4b69095693c5b0d78c73f8c
sj2sj <waterttug@naver.com>
2021-09-30 15:36:24 +0900

    boot_nav.jsp 링크 수정/list.jsp css

--- src/main/java/com/iu/s5/board/community/CommunityController.java
+++ src/main/java/com/iu/s5/board/community/CommunityController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,6 +18,13 @@ public class CommunityController {
 
 	@Autowired
 	private CommunityService communityService;
+	
+	
+	@ModelAttribute("board")
+	public String getBoard() {
+		return "community";
+	}
+	
 	
 	@GetMapping("select")
 	public ModelAndView getSelect(BoardDTO boardDTO) throws Exception {
