7ea67432b7fad6e676cb1884224d0e4c37892824
DANGHYEON <rlaalsrl3023@naver.com>
2021-10-05 10:27:07 +0900

    search

--- src/main/java/com/iu/s5/HomeController.java
+++ src/main/java/com/iu/s5/HomeController.java
@@ -8,8 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
@@ -36,6 +38,16 @@ private static final Logger logger = LoggerFactory.getLogger(HomeController.clas
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@GetMapping("search")
+	public ModelAndView search() throws Exception{
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("search/searchList");
+		
+		return mv;
+		
 	}
 	
 	
--- src/main/webapp/WEB-INF/views/search/searchList.jsp
+++ src/main/webapp/WEB-INF/views/search/searchList.jsp
@@ -1,12 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<c:import url="../temp/boot_head.jsp"></c:import>
 </head>
 <body>
+<c:import url="../temp/boot_nav.jsp"></c:import>
+
+
+
+
+
 	<h1>Search Page</h1>
 
 </body>
