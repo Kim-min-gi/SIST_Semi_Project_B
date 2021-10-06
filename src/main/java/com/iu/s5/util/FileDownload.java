ba515a9423ff2665bf2991d9e2a38040c5068661
sj2sj <waterttug@naver.com>
2021-10-06 11:38:45 +0900

    add FileDownload.java

---
+++ src/main/java/com/iu/s5/util/FileDownload.java
@@ -0,0 +1,24 @@
+package com.iu.s5.util;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+
+@Component
+public class FileDownload extends AbstractView {
+
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		//폴더명
+		String board = (String) model.get("board");
+	}
+	
+}
