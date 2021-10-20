ef94a25fda1727f9385a051ede2e2264328d1ac2
sj2sj <waterttug@naver.com>
2021-10-20 17:50:53 +0900

    review

--- src/main/java/com/iu/s5/review/ReviewDAO.java
+++ src/main/java/com/iu/s5/review/ReviewDAO.java
@@ -1,6 +1,7 @@
 package com.iu.s5.review;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +14,29 @@ public class ReviewDAO {
 	private SqlSession sqlSession;
 	private final String NAMESPACE = "com.iu.s5.review.ReviewDAO.";
 	
+	/* 리뷰 이미지 파일 처리 */
+	public int setReviewFile(ReviewFilesDTO reviewFilesDTO) throws Exception {
+		return sqlSession.insert(NAMESPACE + "setReviewFile", reviewFilesDTO);
+	}
+	
 	
 	//식당별 평균 평점
 	public float getRatingAvg(ReviewDTO reviewDTO) throws Exception {
 		return sqlSession.selectOne(NAMESPACE + "getRatingAvg", reviewDTO);
 	}
 	
+	//식당 총 리뷰 수
+	public Long getReviewCount(ReviewDTO reviewDTO) throws Exception {
+		return sqlSession.selectOne(NAMESPACE + "getReviewCount", reviewDTO);
+	}
+	
 	//리뷰 보기
-	public void getReviewList() throws Exception {
+	public List<ReviewDTO> getReviewList(Map<String, Object> map) throws Exception {
+		return sqlSession.selectList(NAMESPACE + "getReviewList", map);
 	}
 	
 	//리뷰 수정
-	public int setUpdate() throws Exception {
+	public int setUpdate(ReviewDTO reviewDTO) throws Exception {
 		return 0;
 	}
 	
