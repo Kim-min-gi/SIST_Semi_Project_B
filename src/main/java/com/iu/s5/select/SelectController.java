eb371b3c3890746d136e69e7f620f548c3066478
abcbcdyl <86539734+abcbcdy@users.noreply.github.com>
2021-10-20 16:59:56 +0900

    1

--- src/main/java/com/iu/s5/select/SelectController.java
+++ src/main/java/com/iu/s5/select/SelectController.java
@@ -2,6 +2,7 @@ package com.iu.s5.select;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,10 +16,12 @@ public class SelectController {
 		private SelectService selectService;
 	
 		//식당 상세페이지
+		@GetMapping("select")
 		public ModelAndView getSelectRT(RestaurantsDTO restaurantsDTO) throws Exception {
 			ModelAndView mv = new ModelAndView();
 			restaurantsDTO = selectService.getSelectRT(restaurantsDTO);
-			
+			mv.addObject("dto", restaurantsDTO);
+			mv.setViewName("restaurants/select");
 			return mv;
 		}
 		
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -50,7 +50,7 @@
 				<ul class="grid swipe-down" id="grid">
 				<c:forEach items="${starVal}" var="s" varStatus="status">
 					<c:forEach items="${s.files}" var="f">
-					<li><a id="sa${status.count}" href="#"><img src="./resources/img/dummy.png" alt="dummy">
+					<li><a id="sa${status.count}" href="/restaurants/select/restNum=${s.restNum}"><img src="./resources/img/dummy.png" alt="dummy">
 					<span id="res">
 					<h3>${s.restName} <span id="starVal">${s.starVal}</span></h3>
 					</span>
