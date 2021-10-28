701d45b525e01727b9b3bfb8daf3af8ac0a1d700
abcbcdyl <86539734+abcbcdy@users.noreply.github.com>
2021-10-28 16:33:10 +0900

    getReviewFiles 추가

--- src/main/java/com/iu/s5/select/SelectDAO.java
+++ src/main/java/com/iu/s5/select/SelectDAO.java
@@ -1,10 +1,13 @@
 package com.iu.s5.select;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.iu.s5.restaurant.RestaurantsDTO;
+import com.iu.s5.review.ReviewFilesDTO;
 
 @Repository
 public class SelectDAO {
@@ -12,6 +15,11 @@ public class SelectDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	private final String NAMESPACE = "com.iu.s5.select.SelectDAO.";
+	
+	//리뷰 사진 가져오기
+	public List<ReviewFilesDTO> getReviewFiles(com.iu.s5.select.RestaurantsDTO restaurantsDTO) throws Exception {
+		return sqlSession.selectList(NAMESPACE+"getReviewFiles", restaurantsDTO);
+	}
 	
 	//카테고리명 호출
 	public String getCategoryName(com.iu.s5.select.RestaurantsDTO restaurantsDTO) throws Exception {
